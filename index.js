f559cc127b059fcb095711e0f6a8699e4d3117b7
VUNico <jiminaleejung@gmail.com>
2020-05-06 16:25:25 +1200

    install babel and nodemon

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const express = require("express");
+import express from "express";
 const app = express();
 const PORT = 4000;
 
-function handleListening() {
+const handleHome = (req, res) => res.send("Hello I am at home!");
+
+const handleListening = () =>
   console.log("Listening on: http://localhost:4000");
-}
+
+app.get("/", handleHome);
 
 app.listen(PORT, handleListening);
