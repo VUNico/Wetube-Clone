eac3e77852dace86a7d26287afe2f29bc5dacc00
VUNico <jiminaleejung@gmail.com>
2020-05-07 13:50:00 +1200

    install middlewares

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import bodyparser from "body-parser";
+import cookieparser from "cookie-parser";
+
 const app = express();
 const PORT = 4000;
 
@@ -6,7 +11,7 @@ const handleHome = (req, res) => res.send("Hello I am at home!");
 
 const handleProfile = (req, res) => res.send("This is your profile.");
 
-/* It is called middleware */
+/* This is called middleware */
 const handleMiddle = (req, res, next) => {
   console.log("Between");
   next();
@@ -15,9 +20,14 @@ const handleMiddle = (req, res, next) => {
 const handleListening = () =>
   console.log("Listening on: http://localhost:4000");
 
-app.get("/", handleHome);
-
+app.use(cookieparser());
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
 app.use(handleMiddle);
+
+app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
 
