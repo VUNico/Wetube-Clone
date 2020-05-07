81f79b822faeb10c5af69b94e3bfc3e6fd44b24a
VUNico <jiminaleejung@gmail.com>
2020-05-07 12:52:19 +1200

    insert middleware

--- index.js
+++ index.js
@@ -4,9 +4,21 @@ const PORT = 4000;
 
 const handleHome = (req, res) => res.send("Hello I am at home!");
 
+const handleProfile = (req, res) => res.send("This is your profile.");
+
+/* It is called middleware */
+const handleMiddle = (req, res, next) => {
+  console.log("Between");
+  next();
+};
+
 const handleListening = () =>
   console.log("Listening on: http://localhost:4000");
 
 app.get("/", handleHome);
+
+app.use(handleMiddle);
+
+app.get("/profile", handleProfile);
 
 app.listen(PORT, handleListening);
