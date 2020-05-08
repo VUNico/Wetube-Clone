a93d330b89b6ba04def220d63a2499bc43cbf4ec
VUNico <jiminaleejung@gmail.com>
2020-05-08 20:51:29 +1200

    modify routers

--- app.js
+++ app.js
@@ -3,29 +3,28 @@ import morgan from "morgan";
 import helmet from "helmet";
 import bodyparser from "body-parser";
 import cookieparser from "cookie-parser";
-import { userRouter } from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 
 const app = express();
 
-const handleHome = (req, res) => res.send("Hello I am at home!");
-
-const handleProfile = (req, res) => res.send("This is your profile.");
-
-/* This is called middleware */
+/* This is called middleware 
 const handleMiddle = (req, res, next) => {
   console.log("Between");
   next();
 };
+app.use(handleMiddle); */
 
 app.use(cookieparser());
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
-app.use(handleMiddle);
 
-app.get("/", handleHome);
-app.get("/profile", handleProfile);
-app.use("/user", userRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
